d0b980026687496b07495799f70f1c4e40ebb0e0
erfan-taheri <erfan8104taheri@gamil.com>
2025-08-08 22:12:49 +0330

    add search box

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -4,22 +4,25 @@ import { IProductList } from "./../../components/ProductItem";
 import ProductItem from "./../../components/ProductItem";
 import Link from "next/link";
 import Pagination from "./../../components/Pagination";
-
+import Search from "./../../components/Search";
 interface IStoreProps {
   params: Promise<{}>;
-  searchParams: Promise<{ page: string; per_page: string }>;
+  searchParams: Promise<{ page: string; per_page: string; title: string }>;
 }
 
 export default async function Store({ searchParams }: IStoreProps) {
+  
   const page = (await searchParams).page ?? "1";
   const per_page = (await searchParams).per_page ?? "5";
+  const title = (await searchParams).title ?? "";
   const result = await fetch(
-    `http://localhost:3000/products?_page=${page}&_per_page=${per_page}`
+    `http://localhost:3000/products?_page=${page}&_per_page=${per_page}&title=${title}`
   );
   const data = (await result.json()) as IProductList[];
   return (
     <Container>
       <h1 className="text-right py-4">فروشگاه </h1>
+      <Search />
       <div className="grid grid-cols-4 gap-1">
         {data.data.map((item) => (
           <Link key={item.id} href={`/store/${item.id}`}>
--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,13 +1,19 @@
 "use client";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import React from "react";
 import ReactPaginate from "react-paginate";
 
 function Pagination({ pageCount }: { pageCount: number }) {
   const router = useRouter();
+  const searchParams = useSearchParams();
+
   const handlePageClick = (e: { selected: number }) => {
     const page = e.selected + 1;
-    router.push(`/store?page=${page}&per_page=5`);
+    const currentSearchParams = new URLSearchParams(searchParams.toString());
+    currentSearchParams.set("page", page.toString());
+    currentSearchParams.set("per-page", "2");
+
+    router.push(`/store?${currentSearchParams.toString()}`);
   };
   return (
     <div>
